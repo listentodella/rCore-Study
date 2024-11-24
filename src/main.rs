2264810e23654f4eeeb602a45e9a37a594efc7c5
leo <leo233.huang@gmail.com>
2024-11-24 21:57:19 +0800

    启用log crate

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,29 @@ mod lang_items;
 mod sbi;
 
 use core::{arch::global_asm, panic};
+use log::{debug, error, info, trace, warn, Level, LevelFilter, Metadata, Record, SetLoggerError};
+
+struct SimpleLogger;
+
+impl log::Log for SimpleLogger {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        metadata.level() <= Level::Info
+    }
+
+    fn log(&self, record: &Record) {
+        if self.enabled(record.metadata()) {
+            println!("{} - {}", record.level(), record.args());
+        }
+    }
+
+    fn flush(&self) {}
+}
+
+static LOGGER: SimpleLogger = SimpleLogger;
+
+fn log_init() -> Result<(), SetLoggerError> {
+    log::set_logger(&LOGGER).map(|()| log::set_max_level(LevelFilter::Trace))
+}
 
 // include_str! 宏, 可以将指令路径下的文件转化为字符串
 // 再通过global_asm!宏嵌入到代码中
@@ -16,12 +39,46 @@ global_asm!(include_str!("entry.asm"));
 // 避免编译器对函数名称进行混淆, 否则链接时, entry.asm将找不到该函数
 #[no_mangle]
 pub fn rust_main() -> ! {
+    extern "C" {
+        fn stext(); // begin addr of text segment
+        fn etext(); // end addr of text segment
+        fn srodata(); // start addr of Read-Only data segment
+        fn erodata(); // end addr of Read-Only data ssegment
+        fn sdata(); // start addr of data segment
+        fn edata(); // end addr of data segment
+        fn sbss(); // start addr of BSS segment
+        fn ebss(); // end addr of BSS segment
+        fn boot_stack_lower_bound(); // stack lower bound
+        fn boot_stack_top(); // stack top
+    }
     clear_bss();
-    println!("hello rust rcore!");
+    println!("[kernel] hello rCore!");
+
+    log_init().unwrap();
+
+    trace!(
+        "[kernel] .text [{:#x}, {:#x})",
+        stext as usize,
+        etext as usize
+    );
+    debug!(
+        "[kernel] .rodata [{:#x}, {:#x})",
+        srodata as usize, erodata as usize
+    );
+    info!(
+        "[kernel] .data [{:#x}, {:#x})",
+        sdata as usize, edata as usize
+    );
+    warn!(
+        "[kernel] boot_stack top=bottom={:#x}, lower_bound={:#x}",
+        boot_stack_top as usize, boot_stack_lower_bound as usize
+    );
+    error!("[kernel] .bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+
     // 如果以panic等非正常途径的方式进入发散
     // make 检查返回值会报错, 属于正常现象
-    panic!("Manually Shutdown the Machine!");
-    //loop {}
+    //panic!("Manually Shutdown the Machine!");
+    sbi::shutdown(false)
 }
 
 fn clear_bss() {
