d272d61f78576f8107c13530b7f2dee2b0c9cdf8
leo <leo233.huang@gmail.com>
2024-12-09 15:32:16 +0800

    mysbi base code, 注意使用最新的riscv crate,否则容易遇到编译问题?

--- myos/sbi/src/main.rs
+++ myos/sbi/src/main.rs
@@ -1,4 +1,41 @@
 #![no_std]
 #![no_main]
 
+use core::arch::asm;
+use core::arch::global_asm;
+use riscv::register::{mepc, mstatus, satp, sie, stvec};
+
 mod lang_item;
+
+global_asm!(include_str!("sbi_boot.S"));
+
+const FW_JUMP_ADDR: usize = 0x8020_0000;
+
+#[no_mangle]
+fn sbi_main() {
+    // 设置跳转模式为S模式
+    let mut val = mstatus::read();
+    val.set_mpp(mstatus::MPP::Supervisor);
+    val.set_mpie(false);
+
+    // 设置M模式的异常程序计数器, 用于 mret 跳转
+    mepc::write(FW_JUMP_ADDR);
+
+    unsafe {
+        // 设置S模式的异常向量表入口地址
+        stvec::write(FW_JUMP_ADDR, stvec::TrapMode::Vectored);
+
+        // 关闭S模式的中断
+        sie::clear_ssoft();
+        sie::clear_sext();
+        sie::clear_stimer();
+    }
+
+    // 关闭S模式的页表转换
+    satp::write(0);
+
+    unsafe {
+        // 切换到S模式
+        asm!("mret");
+    }
+}
