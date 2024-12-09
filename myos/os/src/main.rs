3909ecaf820ee45260120f2895ee7f26ae49782d
leo <leo233.huang@gmail.com>
2024-12-09 16:42:10 +0800

    S-mode kernel main supported

--- myos/os/src/main.rs
+++ myos/os/src/main.rs
@@ -1,4 +1,17 @@
 #![no_std]
 #![no_main]
 
+use core::arch::global_asm;
+
 mod lang_item;
+
+global_asm!(include_str!("boot.S"));
+
+#[no_mangle]
+fn kernel_main() -> ! {
+    let a = 12u8;
+    let b = 32u8;
+    let _c = a + b;
+
+    loop {}
+}
