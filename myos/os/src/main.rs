3cbad6ec924d80e79fb2e7cebb191556c2368b35
leo <leo233.huang@gmail.com>
2024-12-15 19:25:52 +0800

    reconsturct the test file tree

--- myos/os/src/main.rs
+++ myos/os/src/main.rs
@@ -8,58 +8,12 @@ mod console;
 mod lang_item;
 mod uart;
 
+mod base;
+
 global_asm!(include_str!("boot.S"));
-global_asm!(include_str!("asm_test.S"));
-
-unsafe fn load_store_embedded() {
-    asm!(
-        "
-        li t0, 0x80200000
-
-        lb t1, (t0)
-        lb t1, 4(t0)
-        lb t1, -4(t0)
-        ld t1, (t0)
-        lb t1, 4(t0)
-
-        lui t0, 0x80200
-        lui t1, 0x40200
-
-        ret
-"
-    );
-}
-
-#[no_mangle]
-unsafe fn load_store_single_step() {
-    //     asm!(
-    //         "
-    // .align 3
-    // .globl my_test_data
-    // my_test_data:
-    // 	.dword 0x12345678abcdabcd
-    // "
-    //     );
-    //let my_test_data = 0x12345678abcdabcdu64;
-
-    asm!("li t0, 0x80200000");
-    asm!("lb t1, (t0)   ");
-    asm!("lb t1, 4(t0)  ");
-    asm!("lb t1, -4(t0) ");
-    asm!("ld t1, (t0)   ");
-    asm!("lb t1, 4(t0)  ");
-    asm!("lui t0, 0x80200");
-    asm!("lui t1, 0x40200");
-    asm!("la  t0, my_test_data");
-    asm!("lla t1, my_test_data");
-    asm!("ret                 ");
-}
 
 #[no_mangle]
 fn kernel_main() -> ! {
-    extern "C" {
-        fn load_store_test();
-    }
     let a = 12u8;
     let b = 32u8;
     let c = a + b;
@@ -73,9 +27,9 @@ fn kernel_main() -> ! {
     println!(" hello myOS, {}", c);
 
     unsafe {
-        load_store_test();
-        load_store_embedded();
-        load_store_single_step();
+        base::load_store::global_asm_test();
+        base::load_store::asm_all();
+        base::load_store::asm_single();
     }
 
     loop {}
