c52ea0f4a83a42a3cb3f7cbdd79b26628937518f
leo233 <leo233.huang@gmail.com>
2025-01-02 19:52:40 +0800

    fix: should not access M csr in S mode

--- myos/os/src/base/csr.rs
+++ myos/os/src/base/csr.rs
@@ -4,17 +4,17 @@ pub unsafe fn csrrw() {
     //csrrw rd, csr, rs
     // csr -> tmp, rs->csr, tmp->rd
     asm!("li a1, 0xFFFF");
-    asm!("csrrw a0, mscratch, a1");
+    asm!("csrrw a0, sscratch, a1");
     asm!("li a0, 0xAAAA");
-    asm!("csrrw a0, mscratch, a0");
+    asm!("csrrw a0, sscratch, a0");
 
     // 如果rd与rs相同, 则起到 scratch 与 rs交换的作用
-    asm!("csrrw sp, mscratch, sp");
+    asm!("csrrw sp, sscratch, sp");
     // 再次调用就可以将rs备份的值取回来(前提是csr没被动过)
-    asm!("csrrw sp, mscratch, sp");
+    asm!("csrrw sp, sscratch, sp");
 
     // csrw 则是伪指令, rd 其实是 x0
     // 效果是 rs->csr
     asm!("li a1, 0xBBBB");
-    asm!("csrw mscratch, a1");
+    asm!("csrw sscratch, a1");
 }
