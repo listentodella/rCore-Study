37c693d4e6a25eb434c5082936e5167d42783ca3
leo233 <leo233.huang@gmail.com>
2025-01-02 22:14:03 +0800

    添加异常之系统调用

--- myos/mysbi/src/sbi_trap.rs
+++ myos/mysbi/src/sbi_trap.rs
@@ -1,5 +1,8 @@
 use core::arch::global_asm;
-use riscv::register::{mepc, mie, mstatus, mtvec};
+use riscv::register::{
+    mcause::{self, Trap},
+    mepc, mie, mstatus, mtvec,
+};
 
 global_asm!(include_str!("sbi_entry.S"));
 
@@ -19,5 +22,78 @@ pub fn sbi_trap_init() {
         mie::clear_msoft();
         mie::clear_mtimer();
         mie::clear_mext();
+    }
+}
+
+#[repr(C)]
+struct SbiTrapRegs {
+    mepc: usize,
+    ra: usize,
+    sp: usize,
+    gp: usize,
+    tp: usize,
+    t0: usize,
+    t1: usize,
+    t2: usize,
+    s0: usize,
+    s1: usize,
+    a0: usize,
+    a1: usize,
+    a2: usize,
+    a3: usize,
+    a4: usize,
+    a5: usize,
+    a6: usize,
+    a7: usize,
+    s2: usize,
+    s3: usize,
+    s4: usize,
+    s5: usize,
+    s6: usize,
+    s7: usize,
+    s8: usize,
+    s9: usize,
+    s10: usize,
+    s11: usize,
+    t3: usize,
+    t4: usize,
+    t5: usize,
+    t6: usize,
+    mstatus: usize,
+}
+
+#[no_mangle]
+//pub fn sbi_trap_handler(regs:&mut SbiTrapRegs) {
+// 是否有必要返回可变引用呢?
+pub fn sbi_trap_handler(regs: &mut SbiTrapRegs) -> &mut SbiTrapRegs {
+    let mcause = mcause::read();
+    let ecall_id = regs.a7;
+    match mcause.cause() {
+        Trap::Exception(num) => if num == 0x09 {},
+        Trap::Interrupt(_num) => {}
+    }
+    regs
+}
+
+const SBI_CONSOLE_PUTCHAR: usize = 1;
+const SBI_CONSOLE_GETCHAR: usize = 2;
+fn sbi_ecall_handle(id: usize, reg: &mut SbiTrapRegs) {
+    let mut ret = 0usize;
+    match id {
+        SBI_CONSOLE_PUTCHAR => {
+            ret = 0;
+        }
+        SBI_CONSOLE_GETCHAR => {
+            ret = 0;
+        }
+        _ => {
+            ret = 0;
+        }
+    }
+
+    /* 系统调用返回的是系统调用指令
+    （例如ECALL指令）的下一条指令 */
+    if ret == 0 {
+        reg.mepc += 4;
     }
 }
---
+++ myos/os/src/syscall.rs
@@ -0,0 +1,31 @@
+use core::arch::asm;
+
+/// 所有的syscall都是通过 ecall 指令出发
+/// 约定 a0~a6保存系统调用的参数, 并且 a0 保存系统调用的返回值
+/// a7 用来传递syscall ID
+fn syscall(id: usize, args: [usize; 3]) -> isize {
+    let mut ret: isize;
+    unsafe {
+        asm!(
+            "ecall",
+            inlateout("a0") args[0] => ret,
+            in("a1") args[1],
+            in("a2") args[2],
+            in("a7") id
+        );
+    }
+
+    ret
+}
+
+const SBI_CONSOLE_PUTCHAR: usize = 1;
+const SBI_CONSOLE_GETCHAR: usize = 2;
+pub fn sbi_putchar(c: char) -> isize {
+    syscall(SBI_CONSOLE_PUTCHAR, [c as usize, 0, 0])
+}
+
+pub fn sbi_put_string(str: &str) {
+    str.chars().for_each(|c| {
+        sbi_putchar(c);
+    });
+}
